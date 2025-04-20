08b9353d23e1be79eb5e221a70a9402266f70f33
Corneille <65753539+JCorneilleN@users.noreply.github.com>
2025-04-19 22:46:15 -0700

    Added more files

--- kumba-dev/core/urls.py
+++ kumba-dev/core/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     #path('', views.login_view, name='login'),
     path('signup/', views.signup, name='signup'),
     path('verify/', views.verify_code, name='verify_code'),
-    #path('logout/', views.logout_view, name='logout'),
-    #path('reset-password/', views.reset_password, name='reset_password'),
+    path('logout/', views.logout_view, name='logout'),
+    path('reset-password/', views.reset_password, name='reset_password'),
     #path('home/', views.home, name='home'),
 ]
