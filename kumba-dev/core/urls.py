5b6acc6d30688c81616607f85ff24340980c89d7
Corneille <65753539+JCorneilleN@users.noreply.github.com>
2025-04-20 21:55:11 -0700

    Added more files

--- kumba-dev/core/urls.py
+++ kumba-dev/core/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),     # “/” → login
     path('signup/', views.signup, name='signup'),
     path('verify/', views.verify_code, name='verify_code'),
+    path('home/', views.home, name='home'),
     path('logout/', views.logout_view, name='logout'),
     path('reset-password/', views.reset_password, name='reset_password'),
-    #path('home/', views.home, name='home'),
 ]
+
